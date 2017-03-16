74061b625a2c10129e5dbb24b61931fbab01c349
David E. Kotecki <david.kotecki@gmmail.com>
2017-03-16 18:52:22 -0400

    initial commit

---
+++ js/myjavascript.js
@@ -0,0 +1,7 @@
+// Insert current year into the footer
+function copyrightYear() {
+   var year = new Date().getFullYear();
+   var element = document.getElementById("cr_year");
+   element.innerHTML = "year";
+}
+//
